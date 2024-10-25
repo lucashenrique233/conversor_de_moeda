23ee04f74ea84231e4788613454fe1ed619aab3b
lucas henrique da silva tavares <tavares.l@alunoescolassociais.org.br>
2024-10-25 08:18:09 -0300

    adicionei o codigo javascript

--- form.js
+++ form.js
@@ -4,14 +4,25 @@ document.getElementById('currency-form').addEventListener('submit', function(eve
 
     // Obter valores de entrada informados pelo usuario
     const valor = parseFloat(document.getElementById('amount').value);
-    const daMoeda = document.getElementById('from-currency').value;
-    const paraMoeda = document.getElementById('from-currency').value;
+    const daMoeda = document.getElementById('daMoeda').value;
+    const paraMoeda = document.getElementById('paraMoeda').value;
     
     // Definir taxas de cambio fixas
     const exchangeRates = {
         USD: {BRL: 5.70, EUR: 0.93 },
         BRL: {USD: 5.70, EUR: 0.93 },
-        EUR: {USD: 1.08,  BRL: 6.16 },
+        EUR: {USD: 1.08,  BRL: 6.16 }
     };
+
+    // conversão simples de moeda
+    let valorConvertido;
+    if(daMoeda === paraMoeda){
+        valorConvertido = valor;
+    }else{
+        valorConvertido = valor  * exchangeRates[daMoeda][paraMoeda];
+    }
+
+  const conversão = document.getElementById('conversão');
+
 });
 
